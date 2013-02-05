87f0aea21314143edf0fca5240aa6a6026973055
Andreas Wallinger <andreas.wallinger@gmail.com>
2013-02-05 15:24:52 +0100

    rename to xawremote.lua

--- lua/xawremote.lua
+++ lua/xawremote.lua
@@ -2,10 +2,11 @@ print("Starting xawremote... ")
 serverUrl = "http://localhost:8888/"
 
 function sendRequest(retValue)
-        --request = { retVal = retValue, label = os.getComputerLabel() }
         local label = os.getComputerLabel()
-		
-		local request = serverUrl..label.."?param=blubb"
+		retValue = retValue or "null"
+		local request = serverUrl..label.."?param="..retValue
+		print("-- send request:")
+		print(request)
 		http.request(request)
 end
  
@@ -20,29 +21,31 @@ function waitForAnswer()
 				print(params)
                 if event == "http_failure" then
                         wait()
-                        sendRequest(false)
+                        sendRequest("waiting")
                 else
                    if event == "http_success" then
-                        --local program = params.program
-                        --local args = params.args
+				   
+						output = "success"
                         if program == "wait" then
                                 wait()
                         else
 							param = params.readAll()
-							print ("host: "..host.." params: "..param)
-							f = loadstring(param)
-							f()
+							-- print ("host: "..host.." params: "..param)
+							f = assert(loadstring(param, output))
 							
-							--shell.run("lua", param);
-							
-							print(params.readAll())
-                             --   local retVal = shell.run(program, args)
+							local status = pcall(f)
+							if status then 
+								print("successfully called f")
+							else
+								output = "error"
+							end
+							print("output")
                         end
-                        sendRequest(true)
+                        sendRequest(output)
                   end
                 end
         end
 end
  
-sendRequest(true)
+sendRequest("first")
 waitForAnswer()
